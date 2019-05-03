4681a849dfafb530319c3eedad4d1cf96564f15b
Deepa Kiwne <deepa.kiwne@gmail.com>
2019-05-02 20:51:23 -0700

    Added easy,medium, hard questions. Added game winning functionality

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -15,11 +15,44 @@
 
 # To help you get started, we've provided a sample paragraph that you can use when testing your code.
 # Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
+from __future__ import print_function
 
 sample = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
 adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
 don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
 tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
+
+easy_question = '''A common first thing to do in a language is display
+'Hello __1__!'  In __2__ this is particularly easy; all you have to do
+is type in:
+__3__ "Hello __1__!"
+Of course, that isn't a very useful thing to do. However, it is an
+example of how to output to the user using the __3__ command, and
+produces a program which does something, so it is useful in that capacity.
+
+It may seem a bit odd to do something in a Turing complete language that
+can be done even more easily with an __4__ file in a browser, but it's
+a step in learning __2__ syntax, and that's really its purpose.'''
+
+medium_question = '''A __1__ is created with the def keyword.  You specify the inputs a
+__1__ takes by adding __2__ separated by commas between the parentheses.
+__1__s by default returns __3__ if you don't specify the value to retrun.
+__2__ can be standard data types such as string, integer, dictionary, tuple,
+and __4__ or can be more complicated such as objects and lambda functions.'''
+
+hard_question = '''When you create a __1__, certain __2__s are automatically
+generated for you if you don't make them manually. These contain multiple
+underscores before and after the word defining them.  When you write
+a __1__, you almost always include at least the __3__ __2__, defining
+variables for when __4__s of the __1__ get made.  Additionally, you generally
+want to create a __5__ __2__, which will allow a string representation
+of the method to be viewed by other developers.
+
+You can also create binary operators, like __6__ and __7__, which
+allow + and - to be used by __4__s of the __1__.  Similarly, __8__,
+__9__, and __10__ allow __4__s of the __1__ to be compared
+(with <, >, and ==).'''
+
 
 # The answer for ___1___ is 'function'. Can you figure out the others?
 
@@ -40,6 +73,15 @@ def find_blank(word, blanks):
     return None
 
 def play_game(in_string, blanks, answers):
+
+    print("You will get 5 guesses per problem")
+    print("")
+    print("The current paragraph reads as such:")
+    print("")
+
+    print(in_string)
+
+    game_won = False
     for blank in blanks:
         index = in_string.find(blank)
         
@@ -47,17 +89,34 @@ def play_game(in_string, blanks, answers):
             try_left = 5
             user_answer_correct = False
             while(not user_answer_correct and try_left > 0):
-                user_answer = ask_user("What should be substituted in for " + blank + "? ")
+                user_answer = ask_user("\n\nWhat should be substituted in for " + blank + "? ")
                 if user_answer == answers.get(blank):
+                    print("\nCorrect!" + "\n\n\nThe current paragraph reads as such:\n")
                     in_string = in_string.replace(blank, user_answer)
                     user_answer_correct = True
                 else:
                     try_left -= 1
-                    print("That isn't the correct answer!  Let's try again; you have " + str(try_left) + " trys left!")
+    
+                    if try_left == 0:
+                        print("You've failed too many straight guesses!  Game over!")
+                        return
+
+                    print("That isn't the correct answer!", end='')  
+                    if try_left == 1:
+                        print(" You only have 1 try left!  Make it count!")
+                    else:
+                        print(" Let's try again; you have " + str(try_left) + " trys left!")
+                    print("")
+                    print("The current paragraph reads as such:")
+                    print("")
+                    print(in_string)
+
             print(in_string)
 
         else:
             return
+
+    print("You won!")
 
 # Ask the user functionality
 def ask_user(question):
@@ -83,46 +142,59 @@ def get_user_option():
 
     return option
 
-easy_blanks = ["___1___", "___2___", "___3___", "___4___"]
+easy_blanks = ["__1__", "__2__", "__3__", "__4__"]
 easy_answers = {
-	"___1___" : "function",
-	"___2___" : "arguments",
-	"___3___" : "none",
-	"___4___" : "list"
+	"__1__" : "world",
+	"__2__" : "python",
+	"__3__" : "print",
+	"__4__" : "html"
 }
 
-medium_blanks = ["___1___", "___2___", "___3___", "___4___"]
+medium_blanks = ["__1__", "__2__", "__3__", "__4__"]
 medium_answers = {
-	"___1___" : "function",
-	"___2___" : "arguments",
-	"___3___" : "none",
-	"___4___" : "list"
+	"__1__" : "function",
+	"__2__" : "arguments",
+	"__3__" : "none",
+	"__4__" : "list"
 }
 
-hard_blanks = ["___1___", "___2___", "___3___", "___4___"]
+hard_blanks = ["__1__", "__2__", "__3__", "__4__", "__5__", "__6__", "__7__", "__8__", "__9__", "__10__"]
 hard_answers = {
-	"___1___" : "function",
-	"___2___" : "arguments",
-	"___3___" : "none",
-	"___4___" : "list"
+	"__1__" : "class",
+	"__2__" : "method",
+	"__3__" : "__init__",
+	"__4__" : "instance",
+    "__5__" : "__repr__",
+    "__6__" : "__add__",
+    "__7__" : "__sub__",
+    "__8__" : "__lt__",
+    "__9__" : "__gt__",
+    "__10__" : "__eq__",
 }
 
 option = get_user_option()
 
+question = None
 blanks = None
 answers = None
 if option == 1:
     print("You've chosen easy!")
+    print("")
+    question = easy_question
     blanks = easy_blanks
     answers = easy_answers
 elif option == 2:
     print("You've chosen medium!")
-    blanks = easy_blanks
-    answers = easy_answers
+    print("")
+    question = medium_question
+    blanks = medium_blanks
+    answers = medium_answers
 elif option == 3:
     print("You've chosen hard!")
-    blanks = easy_blanks
-    answers = easy_answers
+    print("")
+    question = hard_question
+    blanks = hard_blanks
+    answers = hard_answers
 
-play_game(sample, blanks, answers)
+play_game(question, blanks, answers)
 
